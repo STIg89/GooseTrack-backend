f4613fe52797b47779552458923d08c0920a48dc
STIg89 <andreystig89@gmail.com>
2023-06-05 13:37:52 +0300

    Update user.js

--- controllers/auth/user.js
+++ controllers/auth/user.js
@@ -128,7 +128,7 @@ const current = async (req, res) => {
   res.status(200).json({
     status: 'success',
     code: 200,
-    data: {
+    user: {
       name: name,
       email: email,
       avatarURL: avatarURL,
