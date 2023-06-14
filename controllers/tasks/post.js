1b59a3bc72902cff0e2afcfa726e5887e59dcf0d
Andruha0013 <andruha0013@gmail.com>
2023-06-14 18:39:17 +0300

    add reminder to task creation (post)

--- controllers/tasks/post.js
+++ controllers/tasks/post.js
@@ -1,13 +1,17 @@
-const Task = require("../../models/task")
+const Task = require('../../models/task');
 const { HttpError, ctrlWrapper } = require('../../helpers');
+const remainder = require('../../helpers/reminder');
 const createTask = async (req, res) => {
-    const { _id: owner } = req.user;
-    const { start, end } = req.body;
-    if (start >= end) {
-        throw HttpError(400, "End time must be greater than Start time.");
-    }
-    const newTask = await Task.create({ ...req.body, owner });
-    res.status(201).json(newTask);
-}
+  const { _id: owner } = req.user;
+  const { start, end } = req.body;
+  if (start >= end) {
+    throw HttpError(400, 'End time must be greater than Start time.');
+  }
+  const newTask = await Task.create({ ...req.body, owner });
 
-module.exports = ctrlWrapper(createTask)
+  await remainder.reminderTask(newTask);
+
+  res.status(201).json(newTask);
+};
+
+module.exports = ctrlWrapper(createTask);
---
+++ helpers/reminder.js
@@ -0,0 +1,50 @@
+const Task = require('../models/task');
+const emailSender = require('./emailSender');
+const schedule = require('node-schedule');
+const { TIME_BEFOR_TASK } = process.env;
+
+const reminderTask = async item => {
+  if (!TIME_BEFOR_TASK) {
+    TIME_BEFOR_TASK.D = 0;
+    TIME_BEFOR_TASK.H = 0;
+    TIME_BEFOR_TASK.M = 5;
+  }
+  console.log('remainder');
+  let time = item.start.split(':');
+  let hour = parseInt(time[0]);
+  let min = time[1];
+
+  const timer = new Date(item.date);
+  timer.setHours(hour, min);
+
+  timer.setDate(time.getDate - TIME_BEFOR_TASK.D);
+  timer.setHours(timer.getHours - TIME_BEFOR_TASK.H);
+  timer.setMinutes(timer.getMinutes() - TIME_BEFOR_TASK.M);
+
+  let job = null;
+  if (item.remindSet == false) {
+    job = schedule.scheduleJob(timer, async () => {
+      const task = await Task.findOneAndUpdate(
+        { _id: item._id, category: 'to-do' },
+        { remindSet: true },
+        { new: true }
+      ).populate('owner', 'name email');
+      console.log(task._id);
+      if (!task || !task.owner) {
+        console.log('off');
+        return;
+      }
+      const letter = `
+            <h1>Hello dear ${task.owner.name}</h1>
+            <p>you have task to do: ${task.title}</p>
+            <p>start at: ${task.start}</p>
+            <p>end at: ${task.end}</p>
+        `;
+      console.log(letter);
+      await emailSender(task.owner.email, 'Goose Remainder', letter);
+    });
+    return job;
+  }
+};
+
+module.exports = { reminderTask };
